7f0cd95c7feff4db5955bb25e3d0d0805499e5e1
imatrix04 <florent.germain040105@orange.fr>
2024-03-12 17:33:08 +0100

    inscription/connexion

---
+++ connexion.js
@@ -0,0 +1,51 @@
+document.getElementById('registerBtn').addEventListener('click', function() {
+    document.getElementById('registerForm').classList.remove('hidden');
+});
+
+document.getElementById('loginBtn').addEventListener('click', function() {
+    document.getElementById('loginForm').classList.remove('hidden');
+});
+
+document.getElementById('registerForm').addEventListener('submit', async function(event) {
+    event.preventDefault();
+
+    const formData = new FormData(this);
+    const email = formData.get('email');
+    const password = formData.get('password');
+
+    try {
+        const response = await fetch('http://192.168.64.194:3000/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ email, password })
+        });
+        const data = await response.json();
+        alert(data.message);
+    } catch (error) {
+        console.error('Error:', error);
+    }
+});
+
+document.getElementById('loginForm').addEventListener('submit', async function(event) {
+    event.preventDefault();
+
+    const formData = new FormData(this);
+    const email = formData.get('email');
+    const password = formData.get('password');
+
+    try {
+        const response = await fetch('http://192.168.64.194:3000/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ email, password })
+        });
+        const data = await response.json();
+        alert(data.message);
+    } catch (error) {
+        console.error('Error:', error);
+    }
+});
